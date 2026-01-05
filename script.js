6676bacda2282e419d7a015b08cef02122ef5d3e
Max_Elder <maxez1999@gmail.com>
2026-01-05 12:12:08 -0300

    Update script.js
    Commit js

--- script.js
+++ script.js
@@ -1,65 +1,41 @@
-$(document).ready(function () {
-  var Scrollbar = window.Scrollbar;
+const Scrollbar = window.Scrollbar;
+Scrollbar.use(window.OverscrollPlugin);
 
-  Scrollbar.use(window.OverscrollPlugin);
+const scrollContainer = document.querySelector('.js-scroll-list');
+const content = document.querySelector('.js-scroll-content');
+const items = document.querySelectorAll('.scroll-list__item');
 
-  var customScroll = Scrollbar.init(document.querySelector('.js-scroll-list'), {
-    plugins: {
-      overscroll: true
-    }
-  });
+const scrollbar = Scrollbar.init(scrollContainer, {
+  damping: 0.08,
+  plugins: {
+    overscroll: true
+  }
+});
 
-  var listItem = $('.js-scroll-list-item');
+// estado inicial
+items[0].classList.add('item-focus');
+if (items[1]) items[1].classList.add('item-next');
 
-  listItem.eq(0).addClass('item-focus');
-  listItem.eq(1).addClass('item-next');
+scrollbar.addListener(({ offset }) => {
+  const scrollTop = offset.y;
+  const containerHeight = scrollContainer.clientHeight;
 
-  customScroll.addListener(function (status) {
+  items.forEach((item, index) => {
+    const itemTop = item.offsetTop;
+    const itemHeight = item.offsetHeight;
 
-    var $content = $('.js-scroll-content');
+    item.classList.remove('item-focus', 'item-next');
 
-    var viewportScrollDistance = 0;
+    // Item em foco
+    if (
+      itemTop < scrollTop + containerHeight / 2 &&
+      itemTop + itemHeight > scrollTop + containerHeight / 2
+    ) {
+      item.classList.add('item-focus');
 
-
-    viewportScrollDistance = status.offset.y;
-    var viewportHeight = $content.height();
-    var listHeight = 0;
-    var $listItems = $content.find('.js-scroll-list-item');
-    for (var i = 0; i < $listItems.length; i++) {
-      listHeight += $($listItems[i]).height();
-    }
-
-    var top = status.offset.y;
-    // console.log(top);
-    var visibleCenterVertical = 0;
-    visibleCenterVertical = top;
-
-    var parentTop = 1;
-    var $lis = $('.js-scroll-list-item');
-    var $focusLi;
-    for (var i = 0; i < $lis.length; i++) {
-      var $li = $($lis[i]);
-      var liTop = $li.position().top;
-      var liRelTop = liTop - parentTop;
-
-      var distance = 0;
-      var distance = Math.abs(top - liRelTop);
-      var maxDistance = $('.js-scroll-content').height() / 2;
-      var distancePercent = distance / (maxDistance / 100);
-
-
-      if (liRelTop + $li.parent().scrollTop() > top) {
-        if (!$li.hasClass('item-focus')) {
-          $li.prev().addClass('item-hide');
-          $lis.removeClass('item-focus');
-          $lis.removeClass('item-next');
-        }
-        $li.removeClass('item-hide');
-        $li.addClass('item-focus');
-        $li.next().addClass('item-next');
-        break;
+      if (items[index + 1]) {
+        items[index + 1].classList.add('item-next');
       }
     }
   });
-
 });
